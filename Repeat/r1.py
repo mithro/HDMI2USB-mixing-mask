79934a0a7d0cf6b898be7cb8662cf04650411288
ssk1328 <shashankgangrade@gmail.com>
2016-06-23 18:53:08 +0530

    Updated implementation of Repeat class in r1.py

---
+++ Repeat/r1.py
@@ -0,0 +1,42 @@
+
+from PIL import Image
+import numpy as np     # numpy
+from collections import namedtuple
+
+_RepeatBase = namedtuple('repeat', ['count','contents'])
+
+class Repeat(_RepeatBase):
+
+	def generate(self):
+		return self.count*self.contents.gen()
+
+
+class Pixel (object):
+	"""Pixel class which has a generate function which returns itself"""
+	def __init__(self,pixel_val):
+		self.pixel_val = pixel_val
+
+	def gen(self) :
+		mylist = []
+
+		if type(self.pixel_val) == int :
+			mylist.append(self.pixel_val)
+
+		elif type(self.pixel_val) == list :
+			for i in self.pixel_val:
+				mylist = mylist + i.generate()
+		else :
+			mylist.append(self.pixel_val.generate())	
+
+		return mylist
+
+BLACK = Pixel(1)
+WHITE = Pixel(255)
+
+r1 = Repeat(1,BLACK)
+r2 = Repeat(3,WHITE)
+
+p2 = Pixel([r1,r2])
+r = Repeat(2, p2)
+print(r.generate())
+
